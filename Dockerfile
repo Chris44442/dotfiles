0cb011df35adffe4bc56232bd17d227f399b429f
christoph bothe <christoph_bothe@gmx.de>
2024-02-05 20:35:44 +0100

    working on install scripts

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM debian:latest
 ENV DEBIAN_FRONTEND noninteractive
 
-RUN apt-get update && apt-get install -y \
+RUN apt-get update && apt-get upgrade && apt-get install -y \
     git
+
+RUN git clone https://github.com/Chris44442/HowToDocs.git
+
+RUN chmod +x HowToDocs/install_scripts/core.sh
+# RUN HowToDocs/install_scripts/core.sh
 
--- install_scripts/core.sh
+++ install_scripts/core.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-apt install curl
-apt install git
+apt-get install -y curl
+apt-get install -y git
 
-apt install build-essential
-apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-export PATH="/root/.cargo/bin:${PATH}"
+# apt-get install -y build-essential
+# apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+# curl https://sh.rustup.rs -sSf | sh -s -- -y
+# export PATH="/root/.cargo/bin:${PATH}"
 
-cargo install alacritty
-cargo install bat
-cargo install fd-find
-cargo install exa
-cargo install ripgrep
-cargo install du-dust
+# cargo install alacritty
+# cargo install bat
+# cargo install fd-find
+# cargo install exa
+# cargo install ripgrep
+# cargo install du-dust
 
 
-wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
-sudo mv thorium.list /etc/apt/sources.list.d/
-sudo apt update
-sudo apt install thorium-browser
+# wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
+# sudo mv thorium.list /etc/apt/sources.list.d/
+# sudo apt-get update
+# sudo apt-get install -y thorium-browser
 
