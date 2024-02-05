4250317ad2e6d361c1becfcb2b828de6e8aed0e5
christoph bothe <christoph_bothe@gmx.de>
2024-02-05 21:32:06 +0100

    working on install scripts

--- install_scripts/core.sh
+++ install_scripts/core.sh
@@ -5,16 +5,21 @@ apt-get install -y git
 
 apt-get install -y build-essential
 apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+apt-get install -y libssl-dev
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="/root/.cargo/bin:${PATH}"
 
-# cargo install alacritty
-# cargo install bat
-# cargo install fd-find
-# cargo install exa
-# cargo install ripgrep
-# cargo install du-dust
+cargo install alacritty
+cargo install bat
+cargo install fd-find
+cargo install exa
+cargo install ripgrep
+cargo install du-dust
+cargo install tmux-sessionizer
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all
+# TODO remove .fzf dir and mv fzf keybindings.bash to .config dir
 
 # wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
 # sudo mv thorium.list /etc/apt/sources.list.d/
