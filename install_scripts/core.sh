b19ffaa2dbfca232bcfbcff68e838d376de7b05f
christoph bothe <christoph_bothe@gmx.de>
2024-02-05 20:21:25 +0100

    working on install scripts

---
+++ install_scripts/core.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt install curl
+apt install git
+
+apt install build-essential
+apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+export PATH="/root/.cargo/bin:${PATH}"
+
+cargo install alacritty
+cargo install bat
+cargo install fd-find
+cargo install exa
+cargo install ripgrep
+cargo install du-dust
+
+
+wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
+sudo mv thorium.list /etc/apt/sources.list.d/
+sudo apt update
+sudo apt install thorium-browser
+
