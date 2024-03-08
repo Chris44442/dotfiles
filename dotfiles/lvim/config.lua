edd72cd57cad0a23ec4d088ff1cf42f6a1e82ec6
christoph bothe <christoph_bothe@gmx.de>
2024-03-08 23:47:28 +0100

    minor addition to dotfile

--- dotfiles/lvim/config.lua
+++ dotfiles/lvim/config.lua
@@ -10,10 +10,8 @@ lvim.plugins = {
 
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<PageUp>', '<Nop>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<PageUp>', '<Nop>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<PageDown>', '<Nop>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<PageDown>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('', '<PageUp>', '<Nop>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('', '<PageDown>', '<Nop>', { noremap = true, silent = true})
 
 -- Render Whitespaces
 vim.cmd[[set list]]
