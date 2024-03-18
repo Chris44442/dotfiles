ac5ebe124ac3acb63fb3df664013758c8bca749d
christoph bothe <christoph_bothe@gmx.de>
2024-03-19 00:26:24 +0100

    arch things

---
+++ install_scripts/02_neovim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "########################"
+echo "###  INSTALL NEOVIM  ###"
+echo "########################"
+wget -P ~/.local https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+cd ~/.local/
+tar xzvf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+cd
+export PATH="/home/chris/.local/nvim-linux64/bin:${PATH}"
+echo "export PATH=\"/home/chris/.local/nvim-linux64/bin:\${PATH}\"" >> /home/chris/.bashrc
+
+cd ./local
+# TODO lunarvim
+
