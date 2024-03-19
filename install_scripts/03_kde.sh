f5af71d2c54f95f7231dc0e437635761f49a79d7
christoph bothe <christoph_bothe@gmx.de>
2024-03-19 23:39:38 +0100

    arch things

--- install_scripts/03_kde.sh
+++ install_scripts/03_kde.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-lookandfeeltool -a org.kde.breezedark.desktop
+# lookandfeeltool -a org.kde.breezedark.desktop
+
+
+curl https://ocs-dl.fra1.cdn.digitaloceanspaces.com/data/files/1558094472/buuf-plasma-21.tar.xz
