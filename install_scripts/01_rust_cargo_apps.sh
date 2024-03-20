aee38b12a5228d882f6fa5b06f88c8c9d2badaa4
christoph bothe <christoph_bothe@gmx.de>
2024-03-20 23:05:43 +0100

    cargo things

--- install_scripts/01_rust_cargo_apps.sh
+++ install_scripts/01_rust_cargo_apps.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-# export PATH="/home/chris/.cargo/bin:${PATH}"
-source "~/.bashrc"
 
-cargo install alacritty
-cargo install bat
-cargo install fd-find
-cargo install exa
-cargo install ripgrep
-cargo install du-dust
-cargo install tmux-sessionizer
-cargo install kalker
+CARGO_PATH="/home/chris/.cargo/bin"
+
+$CARGO_PATH/cargo install alacritty
+$CARGO_PATH/cargo install bat
+$CARGO_PATH/cargo install fd-find
+$CARGO_PATH/cargo install exa
+$CARGO_PATH/cargo install ripgrep
+$CARGO_PATH/cargo install du-dust
+$CARGO_PATH/cargo install tmux-sessionizer
+$CARGO_PATH/cargo install kalker
+$CARGO_PATH/cargo install vhdl_ls
 
