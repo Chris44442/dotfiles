198f2a3d7925de83923fa8cc4215ef668e240c42
christoph bothe <christoph_bothe@gmx.de>
2024-03-20 23:05:47 +0100

    lunarvim things

--- install_scripts/02_neovim.sh
+++ install_scripts/02_neovim.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "########################"
-echo "###  INSTALL NEOVIM  ###"
-echo "########################"
 wget -P ~/.local https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
 cd ~/.local/
 tar xzvf nvim-linux64.tar.gz
@@ -10,13 +7,11 @@ rm nvim-linux64.tar.gz
 cd
 echo "export PATH=\"/home/chris/.local/nvim-linux64/bin:\${PATH}\"" >> /home/chris/.bashrc
 
-echo "##########################"
-echo "###  INSTALL LUNARVIM  ###"
-echo "##########################"
 LUNARVIM_PATH="/home/chris/HowToDocs/install_scripts/lvim"
 LUNARVIM_PATH_DEST="/home/chris/.local/share/lunarvim"
 mkdir -p $LUNARVIM_PATH_DEST
 cp -r $LUNARVIM_PATH $LUNARVIM_PATH_DEST
 mv $LUNARVIM_PATH_DEST/lvim/lvim/lvim ~/.local/nvim-linux64/bin
 mv $LUNARVIM_PATH_DEST/lvim/lvim ~/.config/
+
 
