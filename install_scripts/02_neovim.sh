c7742c0ac54d783ecc5f229313846cd31aaf439d
christoph bothe <christoph_bothe@gmx.de>
2024-03-20 22:24:02 +0100

    lunarvim things

--- install_scripts/02_neovim.sh
+++ install_scripts/02_neovim.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "########################"
-echo "###  INSTALL NEOVIM  ###"
-echo "########################"
-wget -P ~/.local https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
-cd ~/.local/
-tar xzvf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
-cd
-# export PATH="/home/chris/.local/nvim-linux64/bin:${PATH}"
-echo "export PATH=\"/home/chris/.local/nvim-linux64/bin:\${PATH}\"" >> /home/chris/.bashrc
+# echo "########################"
+# echo "###  INSTALL NEOVIM  ###"
+# echo "########################"
+# wget -P ~/.local https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+# cd ~/.local/
+# tar xzvf nvim-linux64.tar.gz
+# rm nvim-linux64.tar.gz
+# cd
+# # export PATH="/home/chris/.local/nvim-linux64/bin:${PATH}"
+# echo "export PATH=\"/home/chris/.local/nvim-linux64/bin:\${PATH}\"" >> /home/chris/.bashrc
 
 
 LUNARVIM_PATH="~/HowToDocs/install_scripts/lunarvim"
