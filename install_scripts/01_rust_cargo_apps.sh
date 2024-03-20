9a77edb33a6728590ec466cd4fb8afbd4727ba40
christoph bothe <christoph_bothe@gmx.de>
2024-03-21 00:26:08 +0100

    cargo and vhdl stuff

--- install_scripts/01_rust_cargo_apps.sh
+++ install_scripts/01_rust_cargo_apps.sh
@@ -18,11 +18,8 @@ cd $CARGO_PATH/..
 mkdir tmp
 cd tmp
 git clone https://github.com/VHDL-LS/rust_hdl.git
-cp rust_hdl/vhdl_libraries ../..
-cd ../..
+cp -rf rust_hdl/vhdl_libraries ..
+cd ..
 rm -rf tmp
-
-
-
 cp ~/HowToDocs/dotfiles/.vhdl_ls.toml ~/.vhdl_ls.toml
 
