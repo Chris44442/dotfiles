9bee2b04c9add3fc0ac6bdcc73aba086afb321a4
christoph bothe <christoph_bothe@gmx.de>
2024-03-20 23:34:18 +0100

    minor stuff

--- install_scripts/03_kde.sh
+++ install_scripts/03_kde.sh
@@ -6,4 +6,6 @@ echo "change colors to breezedark"
 echo "change icons manually, e.g. buuf or candy"
 echo "change window decoration to plastik"
 echo "change login screen"
+echo "shortcut for virtual desktop, alt + f1 etc"
+echo "ctrl alt t shortcut to alacritty"
 
