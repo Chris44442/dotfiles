6c9a3dfcf33d7c190129dc2af4386ffacd32094b
christoph bothe <christoph_bothe@gmx.de>
2024-03-23 00:46:11 +0100

    vhdl sim ls stuff

---
+++ install_scripts/05_vhdl_sim_ls.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip3 install vunit_hdl --break-system-packages
+pip3 install corsair --break-system-packages
+
+git clone https://github.com/nickg/nvc.git
+
+cd nvc
+./autogen.sh
+mkdir build && cd build
+../configure
+make
+make install
+
