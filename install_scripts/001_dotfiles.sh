98ef7cf704902fcdb3bcdd7f1e3abd3625c8812a
christoph bothe <christoph_bothe@gmx.de>
2024-03-23 21:56:54 +0100

    tmux stuff

--- install_scripts/001_dotfiles.sh
+++ install_scripts/001_dotfiles.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-
 cat ~/HowToFiles/dotfiles/.bashrc >> ~/.bashrc
-
 
--- install_scripts/09_tmux.sh
+++ install_scripts/09_tmux.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp ~/HowToDocs/dotfiles/.tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/tms
+cp ~/HowToDocs/dotfiles/tms/config.toml ~/.config/tms/config.toml
 
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
