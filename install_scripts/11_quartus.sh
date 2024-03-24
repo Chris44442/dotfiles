1d5d57b9442b556ba6ceb012d3e234003ba13a1f
christoph bothe <christoph_bothe@gmx.de>
2024-03-24 11:49:17 +0100

    quartus

---
+++ install_scripts/11_quartus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo cp ~/HowToDocs/install_scripts/quartus/libcrypt.so.1 /lib/libcrypt.so.1
+
+echo "MIGHT NEED sudo pacman -S openssl"
+echo "MIGHT NEED sudo pacman -S openssl-1.1"
+echo "MIGHT NEED sudo pacman -S lib32-openssl"
+echo "MIGHT NEED sudo pacman -S lib32-openssl-1.1"
+
