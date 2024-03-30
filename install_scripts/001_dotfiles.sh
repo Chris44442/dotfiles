0722df322b339b5712ee953df158582795a567f7
christoph bothe <hanz_wurzt@web.de>
2024-03-30 12:15:49 +0100

    minor fix

--- install_scripts/001_dotfiles.sh
+++ install_scripts/001_dotfiles.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 cat ~/HowToDocs/dotfiles/.bashrc >> ~/.bashrc
+mkdir -p ~/.config/sway
+mkdir -p ~/.config/waybar
+mkdir -p ~/.config/swaylock
+mkdir -p ~/.config/wofi
 cp ~/HowToDocs/install_scripts/firespin_grey.jpg ~/.config/
+
 
