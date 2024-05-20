d6d1b0789db760c3fd3fad7503f10e64395afb96
christoph bothe <hanz_wurzt@web.de>
2024-05-20 15:32:20 +0200

    fixes

--- install_scripts/07_lazygit.sh
+++ install_scripts/07_lazygit.sh
@@ -4,7 +4,8 @@ mkdir tmp1 && cd tmp1
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-mv lazygit ~/.local/bin/lazygit
+# copy it into cargo because neovim lazygit plugin has issues with path
+mv lazygit ~/.cargo/bin/lazygit
 cd ..
 rm -rf tmp1
 
