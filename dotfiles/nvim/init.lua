92ed0024a922f72e810f8a6904e14c58a0f6ce29
christoph bothe <hanz_wurzt@web.de>
2024-12-29 12:57:45 +0100

    rust fmt stuff in neovim

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -475,6 +475,13 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
   {"nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
